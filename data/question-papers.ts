2c1d914c229cfdbc998ea81eb81e2c9807f8d842
Vedant1703 <80392407+Vedant1703@users.noreply.github.com>
2025-08-22 02:32:50 +0530

    Added exam papers  (#113)
    * Added exam papers for sem1 and sem2 batch 2024
    
    ---------
    
    Co-authored-by: Vedant <vedantkulkarni@vedants-MacBook-Air.local>
    Co-authored-by: Om Swami <reach.oms2004@gmail.com>

--- data/question-papers.ts
+++ data/question-papers.ts
@@ -32,8 +32,8 @@ const questionPapers: TypeQuestionPaper[] = [
       'https://drive.google.com/file/d/1mluV_CQw5lpfOBRAr6T6rPDubV20HAPc/view?usp=drive_link',
   },
   {
-    subject: 'OOPs',
-    subjectCode: 'OOPs',
+    subject: 'Object-Oriented Programming System',
+    subjectCode: 'OOPS',
     batch: 2023,
     semester: 2,
     exam: 'End',
@@ -50,6 +50,156 @@ const questionPapers: TypeQuestionPaper[] = [
     url: 'question_papers/btech/2nd_sem/2023/end/PC.pdf',
     viewUrl:
       'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Computational Thinking through programming',
+    subjectCode: 'CTP',
+    batch: 2024,
+    semester: 1,
+    exam: 'End',
+    url: 'question_papers/btech/1st_sem/2024/end/ctp_end.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Professional Communication',
+    subjectCode: 'PC',
+    batch: 2024,
+    semester: 1,
+    exam: 'End',
+    url: 'question_papers/btech/1st_sem/2024/end/PC_1_end.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'System Programming and Scripting',
+    subjectCode: 'SPS',
+    batch: 2024,
+    semester: 1,
+    exam: 'End',
+    url: 'question_papers/btech/1st_sem/2024/end/sps_end.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Web Development',
+    subjectCode: 'WDA',
+    batch: 2024,
+    semester: 1,
+    exam: 'End',
+    url: 'question_papers/btech/1st_sem/2024/end/wda1_end.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Database Management System',
+    subjectCode: 'DBMS',
+    batch: 2024,
+    semester: 1,
+    exam: 'Mid',
+    url: 'question_papers/btech/1st_sem/2024/mid/dbms_mid_sem.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Professional Communication',
+    subjectCode: 'PC',
+    batch: 2024,
+    semester: 1,
+    exam: 'Mid',
+    url: 'question_papers/btech/1st_sem/2024/mid/pc_1_mid.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Web Development',
+    subjectCode: 'WDA',
+    batch: 2024,
+    semester: 1,
+    exam: 'Mid',
+    url: 'question_papers/btech/1st_sem/2024/mid/wda1_mid.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Computer Org and Architecture',
+    subjectCode: 'COA',
+    batch: 2024,
+    semester: 2,
+    exam: 'End',
+    url: 'question_papers/btech/2nd_sem/2024/end/Coa_end_sem.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Data Structure',
+    subjectCode: 'DS',
+    batch: 2024,
+    semester: 2,
+    exam: 'End',
+    url: 'question_papers/btech/2nd_sem/2024/end/DS_end_sem.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Object-Oriented Programming System',
+    subjectCode: 'OOPS',
+    batch: 2024,
+    semester: 2,
+    exam: 'End',
+    url: 'question_papers/btech/2nd_sem/2024/end/OOPS_end_sem.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Professional Communication',
+    subjectCode: 'PC',
+    batch: 2024,
+    semester: 2,
+    exam: 'End',
+    url: 'question_papers/btech/2nd_sem/2024/end/PC_end_sem.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Computer Org and Architecture',
+    subjectCode: 'COA',
+    batch: 2024,
+    semester: 2,
+    exam: 'Mid',
+    url: 'question_papers/btech/2nd_sem/2024/mid/coa_mid.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Object-Oriented Programming System',
+    subjectCode: 'OOPS',
+    batch: 2024,
+    semester: 2,
+    exam: 'Mid',
+    url: 'question_papers/btech/2nd_sem/2024/mid/oops_mid.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Professional Communication',
+    subjectCode: 'PC',
+    batch: 2024,
+    semester: 2,
+    exam: 'Mid',
+    url: 'question_papers/btech/2nd_sem/2024/mid/PC_2_mid.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
+  },
+  {
+    subject: 'Web Development',
+    subjectCode: 'WDA',
+    batch: 2024,
+    semester: 2,
+    exam: 'Mid',
+    url: 'question_papers/btech/2nd_sem/2024/mid/wda_2_mid.pdf',
+    // viewUrl:
+      // 'https://drive.google.com/file/d/1maMsCx7-R2EQdjUo32iqshmbae5PSSdP/view?usp=drive_link',
   },
   {
     subject: 'Computer Networks',
